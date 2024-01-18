ab44735f358b303c95867f1fc3a251f547c463e5
Raman Krytski <rkeydevelop@gmail.com>
2024-01-18 22:19:50 +0100

    feat: first version of Piece abstraction is implemented

---
+++ src/Game/Piece/PieceAbstraction.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from 'react';
+import { Coordinates, xType, xyType, yType } from '../Coordinates/Coordinates';
+import { Color } from '../types';
+
+export abstract class Piece {
+  readonly component: ReactNode;
+  readonly color: Color;
+
+  private _coordinates!: xyType;
+  private _x!: xType;
+  private _y!: yType;
+
+  constructor(coords: xyType, color: Color) {
+    this.color = color;
+    this.setNewCoords(coords);
+  }
+
+  setNewCoords(coords: xyType) {
+    this._coordinates = coords;
+    this._x = Coordinates.getX(coords);
+    this._y = Coordinates.getY(coords);
+  }
+
+  get coordinates() {
+    return this._coordinates;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  get y() {
+    return this._y;
+  }
+}
