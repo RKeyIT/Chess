fbeadfd0d5acc59b45e080fa4411e22c1ae2e3e1
Raman Krytski <rkeydevelop@gmail.com>
2024-01-18 21:22:03 +0100

    feat: CellComponent was implemented with styles

---
+++ src/Game/Cell/CellComponent.tsx
@@ -0,0 +1,18 @@
+import { Cell } from './CellModel';
+import styles from './CellStyles.module.css';
+
+interface ICellProps {
+  CellModel: Cell;
+}
+
+export function CellComponent({ CellModel }: ICellProps) {
+  const { color, coordinates } = CellModel;
+
+  return (
+    <div className={`${styles.Cell} ${styles[color]}`}>
+      <div className={styles.coordinates}>{coordinates}</div>
+      <div className={styles.highlighter}></div>
+      {/* {piece && piece.component} */}
+    </div>
+  );
+}
