35509e613e8cdd2237ddcd2f68ddeb620a683535
Raman Krytski <rkeydevelop@gmail.com>
2024-01-19 20:57:47 +0100

    fix: render board using it model

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,10 +1,8 @@
 import styles from './Game.module.css';
-import { BoardComponent } from './Board/BoardComponent';
+import { Board } from './Board/BoardModel';
 
 export function Game() {
-  return (
-    <div className={styles.Game}>
-      <BoardComponent></BoardComponent>
-    </div>
-  );
+  const board = Board.getInstanceLink();
+
+  return <div className={styles.Game}>{board.component}</div>;
 }
