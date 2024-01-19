e57d5dbdd71f6d47b85c1cf79e77f989ba700b33
Raman Krytski <rkeydevelop@gmail.com>
2024-01-19 21:52:35 +0100

    feat: link to board field was implemented by LAZY INITIALIZATION PATTERN

--- src/Game/Cell/CellModel.tsx
+++ src/Game/Cell/CellModel.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import { Coordinates, xType, xyType, yType } from '../Coordinates/Coordinates';
 import { Color } from '../types';
 import { CellComponent } from './CellComponent';
+import { Board, IBoardField } from '../Board/BoardModel';
 
 export class Cell {
   readonly component: ReactNode;
@@ -10,12 +11,26 @@ export class Cell {
   readonly y: yType;
   readonly color: Color;
 
+  // NOTE - this field will implemented by "lazy" initialisation by it getter
+  private _boardField!: IBoardField;
+
   constructor(coordinates: xyType) {
     this.coordinates = coordinates;
     this.x = Coordinates.getX(coordinates);
     this.y = Coordinates.getY(coordinates);
     this.color = this.defineColor(this.x, this.y);
-    this.component = <CellComponent CellModel={this} />;
+    this.component = (
+      <CellComponent key={this.color + coordinates} CellModel={this} />
+    );
+  }
+
+  // NOTE - "Lazy initialisation" pattern
+  get boardField() {
+    if (!this._boardField) {
+      this._boardField = Board.getFieldLink(this.coordinates);
+    }
+
+    return this._boardField;
   }
 
   private defineColor(x: xType, y: yType): Color {
