3af513278c640e32c68970196181c910687645fa
Raman Krytski <rkeydevelop@gmail.com>
2024-01-19 20:57:24 +0100

    feat: major Board OOP update

--- src/Game/Board/BoardComponent.tsx
+++ src/Game/Board/BoardComponent.tsx
@@ -1,15 +1,14 @@
-import { Coordinates } from '../Coordinates/Coordinates';
+import { Coordinates, xyType } from '../Coordinates/Coordinates';
 import { Board } from './BoardModel';
 import styles from './BoardStyles.module.css';
 
 export function BoardComponent() {
-  const board = Board.getInstance();
-  const keys = Coordinates.xyArray;
+  const keys: xyType[] = Coordinates.xyArray;
 
   return (
     <div className={styles.Board}>
-      {keys.map((key) => {
-        return board.field[key].cell.component;
+      {keys.map((key: xyType) => {
+        return Board.getFieldLink(key).cell.component;
       })}
     </div>
   );
---
+++ src/Game/Board/BoardModel.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+import { Cell } from '../Cell/CellModel';
+import { Coordinates, xyType } from '../Coordinates/Coordinates';
+import { Pawn } from '../Piece/Pawn/PawnModel';
+import { Piece } from '../Piece/PieceAbstraction';
+import { BoardComponent } from './BoardComponent';
+
+export type BoardTypeObject = Record<xyType, IBoardField>;
+
+export interface IBoardField {
+  cell: Cell;
+  piece: Piece | null;
+}
+
+export class Board {
+  private constructor() {}
+  private static instance: Board = new Board();
+
+  readonly board: BoardTypeObject = Board.createBoard();
+  readonly component: ReactNode = (<BoardComponent />);
+
+  private static createBoard(): BoardTypeObject {
+    const newBoard = {} as BoardTypeObject;
+
+    Coordinates.xyArray.forEach((coords: xyType) => {
+      newBoard[coords] = {} as IBoardField;
+      newBoard[coords].cell = new Cell(coords);
+      newBoard[coords].piece = null;
+    });
+
+    newBoard['C3'].piece = new Pawn('C3', 'white');
+
+    return newBoard;
+  }
+
+  public static getInstanceLink = (): Board => Board.instance;
+
+  public static getFieldLink(coordinates: xyType): IBoardField {
+    return Board.instance.board[coordinates];
+  }
+}
