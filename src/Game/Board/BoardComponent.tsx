0828961087af294b46a4ab61fa0f0f3079154ee8
Raman Krytski <rkeydevelop@gmail.com>
2024-01-20 22:14:47 +0100

    feat: Board re-render feature is done

--- src/Game/Board/BoardComponent.tsx
+++ src/Game/Board/BoardComponent.tsx
@@ -2,80 +2,73 @@ import { useState, MouseEvent, useEffect } from 'react';
 import { Coordinates, xyType } from '../Coordinates/Coordinates';
 import { Board } from './BoardModel';
 import styles from './BoardStyles.module.css';
+import { Piece } from '../Piece/PieceAbstraction';
 
 export function BoardComponent() {
   const board = Board.getInstanceLink().board;
   const coords: xyType[] = Coordinates.xyArray;
 
-  const [currentCoord, setCurrentCoord] = useState<xyType>('A1');
+  const [pickedPiece, setPickedPiece] = useState<Piece | null>(null);
+  const [currentCoord, setCurrentCoord] = useState<xyType | null>(null);
+  const [reRender, forceReRender] = useState(1);
+
+  // SECTION - Force re-render mechanism
+  useEffect(() => {
+    forceReRender(0);
+  }, [pickedPiece]);
 
   useEffect(() => {
-    console.log('useEffect: ' + currentCoord);
-  }, [currentCoord, board]);
+    forceReRender(1);
+  }, [reRender]);
+  // !SECTION
 
-  const mouseDownHandler = (e: MouseEvent) => {
+  const renderBoard = () => {
+    return coords.map((coord: xyType) => {
+      return board[coord].cell.component;
+    });
+  };
+
+  const clickHandler = (e: MouseEvent) => {
     const target = e.target as HTMLDivElement;
     const isHTML = target && target.parentNode instanceof HTMLDivElement;
     const isSVG = target && target.parentNode instanceof SVGElement;
+    const coordinates: xyType | null = getCoords();
 
-    let coords;
+    if (pickedPiece && coordinates) {
+      // const prevCoords = pickedPiece.coordinates;
+      setCurrentCoord(pickedPiece.coordinates);
+      console.log(currentCoord);
 
-    if (isHTML) {
-      coords = target.parentNode.dataset.coordinates as xyType;
+      board[currentCoord!].piece = null;
+      board[coordinates].piece = pickedPiece;
+      console.log(board[currentCoord!], board[coordinates]);
 
-      setCurrentCoord(coords);
-    } else if (isSVG) {
-      coords = (target.parentNode.parentNode as HTMLDivElement).dataset
-        .coordinates as xyType;
+      setCurrentCoord(null);
+      setPickedPiece(null);
+    }
 
-      setCurrentCoord(coords);
-    } else {
-      throw new Error('Unextected else case caused!');
+    if (!pickedPiece && coordinates && board[coordinates].piece) {
+      console.log(board[currentCoord!], board[coordinates]);
+      setCurrentCoord(coordinates);
+      setPickedPiece(board[coordinates].piece);
+    }
+
+    function getCoords(): xyType | null {
+      if (isHTML) {
+        return target.parentNode.dataset.coordinates as xyType;
+      }
+      if (isSVG) {
+        return (target.parentNode!.parentNode as HTMLDivElement).dataset
+          .coordinates as xyType;
+      }
+
+      return null;
     }
   };
-
-  const mouseUpHandler = (e: MouseEvent) => {
-    const target = e.target as HTMLDivElement;
-    const isHTML = target && target.parentNode instanceof HTMLDivElement;
-    const isSVG = target && target.parentNode instanceof SVGElement;
-
-    const getCoords = (): xyType => {
-      return isHTML
-        ? (target.parentNode.dataset.coordinates as xyType)
-        : ((target.parentNode!.parentNode as HTMLDivElement).dataset
-            .coordinates as xyType);
-    };
-
-    const coords = getCoords();
-
-    if (isHTML || isSVG) {
-      const piece = board[currentCoord].piece;
-
-      board[currentCoord].piece = null;
-      board[coords].piece = piece;
-
-      console.log(board[currentCoord]);
-      console.log(board[coords]);
-
-      setCurrentCoord(coords);
-    } else {
-      throw new Error('Unextected else case caused!');
-    }
-  };
-
-  // e.target.offsetParent
-  // e.target.parentElement
-  // e.target.parentNode
 
   return (
-    <div
-      className={styles.Board}
-      onMouseDown={mouseDownHandler}
-      onMouseUp={mouseUpHandler}
-    >
-      {coords.map((coord: xyType) => {
-        return board[coord].cell.component;
-      })}
+    <div className={styles.Board} onClick={clickHandler}>
+      {reRender && renderBoard()}
     </div>
   );
 }
