4f86ca6e3e28bacbd14e5b74407ae1c62db5f82b
Raman Krytski <rkeydevelop@gmail.com>
2024-01-20 09:36:41 +0100

    refactoring: little semantic upgrade for BoardComponent

--- src/Game/Board/BoardComponent.tsx
+++ src/Game/Board/BoardComponent.tsx
@@ -3,12 +3,13 @@ import { Board } from './BoardModel';
 import styles from './BoardStyles.module.css';
 
 export function BoardComponent() {
-  const keys: xyType[] = Coordinates.xyArray;
+  const board = Board.getInstanceLink().board;
+  const coords: xyType[] = Coordinates.xyArray;
 
   return (
     <div className={styles.Board}>
-      {keys.map((key: xyType) => {
-        return Board.getFieldLink(key).cell.component;
+      {coords.map((coord: xyType) => {
+        return board[coord].cell.component;
       })}
     </div>
   );
