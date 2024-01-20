979c4a71512f5c41422bddddb44934ad164da01c
Raman Krytski <rkeydevelop@gmail.com>
2024-01-20 14:31:20 +0100

    feat: added new type enum PieceNames

--- src/Game/types.ts
+++ src/Game/types.ts
@@ -1,3 +1,11 @@
 // General types
 
 export type Color = 'white' | 'black';
+export enum PieceNames {
+  KING = 'king',
+  QUEEN = 'queen',
+  BISHOP = 'bishop',
+  KNIGHT = 'knight',
+  ROOK = 'rook',
+  PAWN = 'pawn',
+}
