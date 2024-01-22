7de3ae19de7b378de0b19a14596d4dfd4357f1b7
Raman Krytski <rkeydevelop@gmail.com>
2024-01-22 14:48:29 +0100

    feat: added available vertical and horizontal moves checker

--- src/Game/Coordinates/Coordinates.ts
+++ src/Game/Coordinates/Coordinates.ts
@@ -1,3 +1,5 @@
+import { Direction } from '../types';
+
 export type xType = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H';
 export type yType = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
 export type xyType = `${xType}${yType}`;
@@ -7,6 +9,31 @@ export class Coordinates {
   public static xArr: xType[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
   public static yArr: yType[] = ['1', '2', '3', '4', '5', '6', '7', '8'];
   public static xyArray: xyType[] = this.createXYArray();
+
+  // Overloads
+  static getNextCoordinate(coord: xType, direction: Direction): xType | null;
+  static getNextCoordinate(coord: yType, direction: Direction): yType | null;
+  static getNextCoordinate(
+    coord: xType | yType,
+    direction: Direction
+  ): xType | yType | null {
+    const index =
+      this.xArr.indexOf(coord as xType) === -1
+        ? this.yArr.indexOf(coord as yType)
+        : this.xArr.indexOf(coord as xType);
+
+    if (index === -1) return null;
+
+    const newIndex = index + direction;
+    const arr: xType[] | yType[] =
+      this.xArr.indexOf(coord as xType) === -1 ? this.yArr : this.xArr;
+
+    return arr[newIndex] || null;
+  }
+
+  public static isAvailableXY(xy: xyType): boolean {
+    return this.indexOf(xy) === -1 ? false : true;
+  }
 
   public static xyStringToArr(xy: xyType): xyTypeAsArray {
     return [xy[0], xy[1]] as xyTypeAsArray;
